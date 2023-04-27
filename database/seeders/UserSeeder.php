44f4bb484cd8cf8cfe2f0f7a9ae32d88df66084d
0x87xan <rh8xv5c1hxpz1u0vco39hzfvgbupx@gmail.com>
2023-04-27 21:06:11 +0300

    refactoring

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -13,6 +14,16 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)->create();
+        $data = file_get_contents(database_path('usernames.txt'));
+        $usernames = explode(' ', $data);
+
+        foreach ($usernames as $username) {
+            User::create([
+                'name' => fake()->name(),
+                'email' => fake()->safeEmail(),
+                'username' => $username,
+                'password' => Hash::make('12345678')
+            ]);
+        }
     }
 }
