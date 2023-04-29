07dfdb6427c1f5f5e414dd0c2d896c91402637e0
0x87xan <rh8xv5c1hxpz1u0vco39hzfvgbupx@gmail.com>
2023-04-29 19:01:46 +0300

    no message

--- app/Http/Requests/Auth/LoginRequest.php
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Requests\Auth;
 
-use App\Models\RestrictedIps;
 use Illuminate\Auth\Events\Lockout;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
