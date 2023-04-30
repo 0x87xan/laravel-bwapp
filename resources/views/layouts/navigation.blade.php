fe2d5b48a2376e000b0339f2dea2fce8a6d30378
0x87xan <rh8xv5c1hxpz1u0vco39hzfvgbupx@gmail.com>
2023-04-30 14:59:32 +0300

    nav link added

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -21,6 +21,11 @@
                         {{ __('Search') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('gallery.view')" :active="request()->routeIs('gallery.view')">
+                        {{ __('Download') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
