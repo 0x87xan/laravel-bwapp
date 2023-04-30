1ae202952abae6bf75d6b46e30114416da20a3b0
0x87xan <rh8xv5c1hxpz1u0vco39hzfvgbupx@gmail.com>
2023-04-30 15:08:41 +0300

    path traversal vulnerability fixed

---
+++ app/Http/Controllers/ImageDownloadController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
+
+class ImageDownloadController extends Controller
+{
+    public function index()
+    {
+        return view('image-download');
+    }
+
+    public function download_image(Request $request)
+    {
+        $filename=basename($request->input('file_name'));
+        if (Storage::exists('public/images/' . $filename)){
+            return response()->download(storage_path('app\\public\\images\\') . $filename);
+        }
+        return redirect()->back()->with('error', "File doesn't exists");
+
+    }
+}
